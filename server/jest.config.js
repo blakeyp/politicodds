1be17f0573fa1f1ac931e440837345fe273b047a
Patrick Blakey <blakeyp@hotmail.co.uk>
2020-06-27 21:40:12 +0100

    Update test coverage config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts']
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!**/index.ts', // Ignore export barrel files
+    '!src/server.ts', // Ignore Express server/config files
+    '!src/app.ts',
+    '!src/router.ts',
+    '!src/config.ts'
+  ]
 }
